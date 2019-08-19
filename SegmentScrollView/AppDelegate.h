8d5513a04c5896ad72382b7db62af3f325a92217
shanyuqin <shanyq@xingyuanauto.com>
2019-08-19 10:13:58 +0800

    Initial Commit

---
+++ SegmentScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentScrollView
+//
+//  Created by shanyuqin on 2019/8/19.
+//  Copyright © 2019 ShanYuQin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
